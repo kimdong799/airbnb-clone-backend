b82e34615cfa1aeaafb0291ade758628375930ad
kimdonghyeon <kimdong799@gmail.com>
2024-01-30 23:08:49 +0900

    make migrate house model

--- houses/admin.py
+++ houses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import House
 # Register your models here.
+
+# House 모델 Admin 패널 추가
+@admin.register(House)
+class HouseAdmin(admin.ModelAdmin):
+    pass
