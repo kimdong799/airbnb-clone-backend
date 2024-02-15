e20721b037682376926fdd1936084c9d8ea524c7
kimdonghyeon <kimdong799@gmail.com>
2024-02-15 22:34:31 +0900

    URL Arguments Done

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.say_hello),
+    path("", views.see_all_rooms),
+    # url로부터 받을 parameter 데이터 타입 명시
+    path("<int:room_id>", views.see_one_room),
+    # 복수의 parameter를 받는 경우
+    path("<int:room_id>/<str:room_name>", views.see_one_room_with_name),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 # Create your views here.
 # view : 유저가 특정 url에 접근했을 때 작동하는 함수
 
 
-def say_hello(request):
-    return HttpResponse(f"Hello {request.user}!")
+def see_all_rooms(request):
+    return HttpResponse(Room.objects.all())
+
+
+def see_one_room(request, room_id):
+    return HttpResponse(Room.objects.get(pk=room_id))
+
+
+def see_one_room_with_name(request, room_id, room_name):
+    return HttpResponse(f"Room ID : {room_id} / {room_name}")
