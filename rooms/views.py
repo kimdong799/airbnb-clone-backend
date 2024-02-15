2169c01aef5342813cc0d75644512294d0b4365b
kimdonghyeon <kimdong799@gmail.com>
2024-02-15 22:47:18 +0900

    render done

--- rooms/views.py
+++ rooms/views.py
@@ -7,7 +7,15 @@ from .models import Room
 
 
 def see_all_rooms(request):
-    return HttpResponse(Room.objects.all())
+    rooms = Room.objects.all()
+    return render(
+        request,
+        "all_rooms.html",
+        {
+            "rooms": rooms,
+            "title": "Hello! this title comes from django",
+        },
+    )
 
 
 def see_one_room(request, room_id):
