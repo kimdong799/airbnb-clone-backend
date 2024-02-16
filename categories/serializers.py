28f66d6245933f1cd7cbaad22b4a7a54aa52f242
kimdonghyeon <kimdong799@gmail.com>
2024-02-16 23:41:24 +0900

    Serializer done

---
+++ categories/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+
+class CategorySerializer(serializers.Serializer):
+    # 노출하고자 하는 필드 정의
+    name = serializers.CharField(required=True)
+    kind = serializers.CharField()
+    created_at = serializers.DateTimeField()
--- categories/views.py
+++ categories/views.py
@@ -1,8 +1,18 @@
-from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Category
+from .serializers import CategorySerializer
 
 
+@api_view()
 def categories(request):
     all_categories = Category.objects.all()
-    return JsonResponse({"ok": True})
+    # 다수의 객체 정보를 직렬화 하기 위해 many 인자 전달
+    serializer = CategorySerializer(all_categories, many=True)
+    return Response(
+        {
+            "ok": True,
+            "categories": serializer.data,
+        }
+    )
