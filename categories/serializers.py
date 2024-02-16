8576cb69327597590a79a4cda65cc801ff39ed3c
kimdonghyeon <kimdong799@gmail.com>
2024-02-17 00:09:59 +0900

    POST Requets done

--- categories/serializers.py
+++ categories/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 
 class CategorySerializer(serializers.Serializer):
     # 노출하고자 하는 필드 정의
+    pk = serializers.IntegerField()
     name = serializers.CharField(required=True)
     kind = serializers.CharField()
     created_at = serializers.DateTimeField()
--- categories/views.py
+++ categories/views.py
@@ -5,14 +5,24 @@ from .models import Category
 from .serializers import CategorySerializer
 
 
-@api_view()
+@api_view(["GET", "POST"])
 def categories(request):
-    all_categories = Category.objects.all()
-    # 다수의 객체 정보를 직렬화 하기 위해 many 인자 전달
-    serializer = CategorySerializer(all_categories, many=True)
-    return Response(
-        {
-            "ok": True,
-            "categories": serializer.data,
-        }
-    )
+    if request.method == "GET":
+        all_categories = Category.objects.all()
+        # 다수의 객체 정보를 직렬화 하기 위해 many 인자 전달
+        serializer = CategorySerializer(all_categories, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        print(request.data)
+        return Response(
+            {
+                "created": True,
+            }
+        )
+
+
+@api_view()
+def category(request, pk):
+    category = Category.objects.get(pk=pk)
+    serializer = CategorySerializer(category, many=False)
+    return Response(serializer.data)
